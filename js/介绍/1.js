f4270020128373681a2541737978a06ce411e771
drowning-in-codes <1322767102@qq.com>
2021-11-06 11:00:48 +0800

    update

---
+++ js/介绍/1.js
@@ -0,0 +1,15 @@
+'use strict';
+// alert("I'm js");
+let message;
+message = 'hello';
+console.log(message);
+let
+    age = 25;
+// message = 'Hello';
+//var和let的差别  声明两次报错
+var user = 'tim';
+var user = "john"; //var可以重复声明
+//const常量
+const myBirthday = '18.04.1982';
+//不能重新赋值
+//一般将常量当作别名,例如颜色
